b612608c0c7dd250e35193f7ea452bd83d5344fa
ChristineWasike <christinewasike22@gmail.com>
2021-12-02 12:15:21 +0300

    New sql file added

--- library.sql
+++ library.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 4.6.6deb5ubuntu0.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 17, 2021 at 01:25 PM
--- Server version: 8.0.27-0ubuntu0.20.04.1
--- PHP Version: 7.4.3
+-- Generation Time: Dec 02, 2021 at 11:14 AM
+-- Server version: 8.0.27
+-- PHP Version: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,90 +23,125 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `BOOK`
+-- Table structure for table `books`
 --
 
-CREATE TABLE `BOOK` (
-  `book_id` int NOT NULL,
-  `book_title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `author_name` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `shelf_id` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `SHELF`
---
-
-CREATE TABLE `SHELF` (
-  `shelf_id` int NOT NULL,
-  `shelf_category` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `USER`
---
-
-CREATE TABLE `USER` (
+CREATE TABLE `books` (
+  `id` int NOT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `author` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `category_id` int NOT NULL,
   `user_id` int NOT NULL,
-  `user_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `password` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `description` varchar(255) NOT NULL,
+  `availability` tinyint NOT NULL,
+  `upload_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=COMPACT;
 
 --
--- Dumping data for table `USER`
+-- Dumping data for table `books`
 --
 
-INSERT INTO `USER` (`user_id`, `user_name`, `password`) VALUES
-(1, 'admin', 'admin');
+INSERT INTO `books` (`id`, `title`, `author`, `category_id`, `user_id`, `description`, `availability`) VALUES
+(1, 'Todo', 'Tada', 6, 1, 'Todo tada', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `description`) VALUES
+(2, 'Finance', '<p>It\'s important to be financially free. Check it!</p>'),
+(5, 'Entrepreneurship', '<p>Entrepreneurship is a great risk on faith, some people are just cut out for it.</p>'),
+(6, 'Life', '<p>Life is a journey</p>'),
+(7, 'Education', '<p>Education is key</p>');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `email` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `password` varchar(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `access` tinyint NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=COMPACT;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `role`, `password`, `access`) VALUES
+(1, 'admin', '', '', 'admin', 0);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `BOOK`
+-- Indexes for table `books`
 --
-ALTER TABLE `BOOK`
-  ADD PRIMARY KEY (`book_id`);
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `SHELF`
+-- Indexes for table `categories`
 --
-ALTER TABLE `SHELF`
-  ADD PRIMARY KEY (`shelf_id`);
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
 
 --
--- Indexes for table `USER`
+-- Indexes for table `users`
 --
-ALTER TABLE `USER`
-  ADD PRIMARY KEY (`user_id`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `BOOK`
+-- AUTO_INCREMENT for table `books`
 --
-ALTER TABLE `BOOK`
-  MODIFY `book_id` int NOT NULL AUTO_INCREMENT;
+ALTER TABLE `books`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
 
 --
--- AUTO_INCREMENT for table `SHELF`
+-- Constraints for table `books`
 --
-ALTER TABLE `SHELF`
-  MODIFY `shelf_id` int NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `USER`
---
-ALTER TABLE `USER`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-COMMIT;
+ALTER TABLE `books`
+  ADD CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
