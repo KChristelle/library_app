94a0c3b89af62e244e02565b1326e3e8d4985362
ChristineWasike <christinewasike22@gmail.com>
2021-12-03 20:27:33 +0300

    Abstraction for the librarian and admin done

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
-<?php include("path.php");?>
-<?php include(ROOT_PATH . "/app/controllers/users.php");?>
+<?php include("path.php"); ?>
+<?php include(ROOT_PATH . "/app/controllers/users.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,12 +9,10 @@
     <title>Register</title>
 
     <!-- Font Awesome CDN -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
     <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
     <!-- Work Sans Link -->
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
@@ -25,64 +23,63 @@
 
 <body>
 
-<!-- Header -->
-<?php include(ROOT_PATH . "/app/includes/header.php");?>
+    <!-- Header -->
+    <?php include(ROOT_PATH . "/app/includes/header.php"); ?>
 
-<!-- Page Wrapper -->
-<div class="wrapper">
+    <!-- Page Wrapper -->
+    <div class="wrapper">
 
-    <!-- Content-->
-            <h2 id="title-login">Register</h2>
+        <!-- Content-->
+        <h2 id="title-login">Register</h2>
 
-            <form action="register.php" method="POST"  id="survey-form">
-                <?php include(ROOT_PATH . "/app/helpers/form_errors.php");?>
+        <form action="register.php" method="POST" id="survey-form">
+            <?php include(ROOT_PATH . "/app/helpers/form_errors.php"); ?>
 
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" class="form-control" name="username" value= "<?php echo $username;?>" id="username">
-                </div>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" name="username" value="<?php echo $username; ?>" id="username">
+            </div>
 
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" class="form-control" value= "<?php echo $email;?>" id="email" >
-                </div>
-                <!-- TODO This is not done in the correct way -->
-                <div class="form-group">
-                    <label for="roles">Role</label>
-                    <select name="role" id="role" class="form-control">
-                        <option value="student">Student</option>
-                        <option value="staff">Staff</option>
-                    </select>
-                </div>
-                
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" class="form-control" name="password" value= "<?php echo $password;?>" id="password" >
-                </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" name="email" value="<?php echo $email; ?>" id="email">
+            </div>
 
-                <div class="form-group">
-                    <label for="conf_password">Confirm Password</label>
-                    <input type="password" class="form-control" name="conf_password" value= "<?php echo $conf_password;?>" id="password" >
-                </div>
+            <div class="form-group">
+                <label for="roles">Role</label>
+                <select name="role" id="role" class="form-control">
+                    <option value="student">Student</option>
+                    <option value="staff">Staff</option>
+                </select>
+            </div>
 
-                <div class="button-submit">
-                    <button type="submit" name = "register-btn" class="btn btn-primary">Register</button>
-                </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" name="password" value="<?php echo $password; ?>" id="password">
+            </div>
 
-                <p style="text-align: center;">Or <a href="login.php">Login</a></p>
-            </form>
+            <div class="form-group">
+                <label for="conf_password">Confirm Password</label>
+                <input type="password" class="form-control" name="conf_password" value="<?php echo $conf_password; ?>" id="password">
+            </div>
 
-</div>
+            <div class="button-submit">
+                <button type="submit" name="register-btn" class="btn btn-primary">Register</button>
+            </div>
 
-<!-- Jquery -->
-<script src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+            <p style="text-align: center;">Or <a href="login.php">Login</a></p>
+        </form>
 
-<!-- Ckeditor -->
-<script src="https://cdn.ckeditor.com/ckeditor5/22.0.0/classic/ckeditor.js"></script>
+    </div>
 
-<!-- JavaScript -->
-<script src="../../assets/js/scripts.js"></script>
+    <!-- Jquery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+    <!-- Ckeditor -->
+    <script src="https://cdn.ckeditor.com/ckeditor5/22.0.0/classic/ckeditor.js"></script>
+
+    <!-- JavaScript -->
+    <script src="../../assets/js/scripts.js"></script>
 </body>
 
 </html>
