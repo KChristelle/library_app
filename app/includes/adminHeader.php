26435e8f1c8f0dcacad3875cca231330fb0dc542
ChristineWasike <christinewasike22@gmail.com>
2021-12-03 20:46:33 +0300

    Added bootstrap js for drop down and logout

--- app/includes/adminHeader.php
+++ app/includes/adminHeader.php
@@ -4,22 +4,20 @@
     <a class="navbar-brand" href="<?php echo BASE_URL . "/index.php"; ?>" class="logo">
         <img src="<?php echo BASE_URL . '/assets/images/book.png'; ?>" style="width:10%; padding-top:.5em; padding-left: 1em;" alt="Logo">
     </a>
-    <!-- <i id="menu" class="fa fa-bars menu-toggle fa-2x"></i> -->
+
 
     <ul class="navbar-nav ml-auto">
         <?php if (isset($_SESSION['id'])) : ?>
-            <li class="dropdown">
-                <!-- <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a> -->
-                <a id="user" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="user" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
                     <i class="fa fa-user" style="font-size: large; color: white;"></i>
                     <?php echo $_SESSION['username'] ?>
-                    <!-- <i class="fa fa-chevron-down" style="font-size: .8em; color: white;"></i> -->
                 </a>
 
-                <ul class="dropdown-menu">
-                    <li><a href="<?php echo BASE_URL . '/admin/dashboard.php'; ?>" class="logout">Dashboard</a></li>
-                    <li><a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout">Logout</a></li>
-                </ul>
+                <div class="dropdown-menu" aria-labelledby="user">
+                    <a href="<?php echo BASE_URL . '/admin/dashboard.php'; ?>" class="logout dropdown-item">Dashboard</a>
+                    <a href="<?php echo BASE_URL . '/logout.php'; ?>" class="logout dropdown-item">Logout</a>
+                </div>
             </li>
         <?php endif; ?>
     </ul>
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
-    $('.menu-toggle').onclick, function () {
-        $('.nav').toggleClass('showing');
-        $('.nav ul').toggleClass('showing');
-    };
+// $(document).ready(function(){
+//     $('.menu-toggle').onclick, function () {
+//         $('.nav').toggleClass('showing');
+//         $('.nav ul').toggleClass('showing');
+//     };
 
-}) 
+// }) 
